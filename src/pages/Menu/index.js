5b40e53f193403f1aafe91fa7d18eb54b4df9378
CHARLES-DESKTOP\user <as130232@gmail.com>
2021-09-06 00:49:59 +0800

    chore 移除註解

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -13,50 +13,6 @@ async function fetchStoreAndMenu(setStore, setGroupMenu) {
 }
 
 const Menu = () => {
-    // const storeData = {
-    //     name: "木魚廚房",
-    //     evaluation: "4.9(500+)",
-    //     delivery: "30",
-    // };
-    // const groupMenuData = [
-    //     {
-    //         id: 1,
-    //         name: "主食",
-    //         items: [
-    //             {
-    //                 id:100,
-    //                 name: "水煮嫩雞胸餐",
-    //                 note: "含營養米飯、水煮蛋、雞胸肉、青菜",
-    //                 price: 100,
-    //             },
-    //             {
-    //                 id:101,
-    //                 name: "火烤雞胸餐",
-    //                 note: "含營養米飯、水煮蛋、雞胸肉、青菜",
-    //                 price: 110,
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "飲料",
-    //         items: [
-    //             {
-    //                 id:102,
-    //                 name: "蘋果奇異果汁",
-    //                 note: "含蘋果、奇異果",
-    //                 price: 70,
-    //             },
-    //             {
-    //                 id:103,
-    //                 name: "香蕉牛奶",
-    //                 note: "含香蕉、牛奶",
-    //                 price: 55,
-    //             },
-    //         ],
-    //     },
-    // ];
-
     const [store, setStore] = useState({});
     const [groupMenu, setGroupMenu] = useState([])
 
@@ -68,7 +24,7 @@ const Menu = () => {
         <div>
             <Store storeDate={store} />
             ---------------
-      <Group groupMenuData={groupMenu} />
+            <Group groupMenuData={groupMenu} />
         </div>
     );
 };
