682e83efa471349908a311451af8cae3005dc24b
Charles Chen <charles@innotech.me>
2021-09-08 21:27:17 +0800

    feat 新增bootstrap

--- src/pages/Menu/components/Group.js
+++ src/pages/Menu/components/Group.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Item from "./Item";
+import { Collapse, Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Group = ({ groupMenuData }) => {
+
   return (
     <>
       {groupMenuData.map((group, idx) => {
         return (
-          <div key={idx}>
-            <h2>{group.name}</h2>
+          <Card className="mt-2">
+            <Card.Header>
+              <h2>{group.name}</h2>
+            </Card.Header>
             {group.items.map((item, idx) => {
               const { id, name, note, price } = item;
               return (
-                <Item key={id} id={id} name={name} note={note} price={price} />
+                <Card.Body className='border-bottom'>
+                  <Item key={id} id={id} name={name} note={note} price={price} />
+                </Card.Body>
               );
             })}
-          </div>
+          </Card>
         );
       })}
     </>
--- src/pages/Menu/components/Item.js
+++ src/pages/Menu/components/Item.js
@@ -1,10 +1,20 @@
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const Item = ({ id, name, note, price }) => {
   return (
-    <div>
-      <h4>{name}</h4>
-      <div>{note}</div>
-      <div>${price}</div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={8}>
+          <h5 ><b>{name}</b></h5>
+          <span>{note}</span>
+          <h7 className="f-w-300 d-flex align-items-center m-b-0">${price}</h7>
+        </Col>
+        <Col xs={4}>
+          <Image src="https://i.imgur.com/R0qpw6h.jpg" fluid rounded />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
