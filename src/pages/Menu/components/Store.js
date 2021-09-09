b45050acb140c5c3bc86c45b2371219780ef7b52
Charles Chen <charles@innotech.me>
2021-09-09 20:56:31 +0800

    feat 更新store component

--- src/pages/Menu/components/Store.js
+++ src/pages/Menu/components/Store.js
@@ -1,13 +1,18 @@
 import { Image } from 'react-bootstrap'
+
+import { Box } from '@material-ui/core';
+import StarRoundedIcon from '@material-ui/icons/StarRounded';
 const Store = ({ storeDate }) => {
   return (
     // <AppBar position="sticky" color="default">
-      
+
     // </AppBar>
     <div>
-      <Image src="https://hsinchu.lakeshore.com.tw/wp-content/uploads/sites/12/2020/08/hs_mwr_gallery_3.jpg" fluid rounded />
+      <Box>
+        <Image src="https://hsinchu.lakeshore.com.tw/wp-content/uploads/sites/12/2020/08/hs_mwr_gallery_3.jpg" fluid rounded />
+      </Box>
       <h1>{storeDate.name}</h1>
-      <p>★{storeDate.rating}</p>
+      <p><StarRoundedIcon color="secondary"></StarRoundedIcon>{storeDate.rating}</p>
       <p>外送 {storeDate.deliveryTime} 分鐘</p>
     </div>
   );
