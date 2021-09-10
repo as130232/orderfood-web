138ef96df9d0ad7e00e62bfca7da0edd5130ef0a
Charles Chen <charles@innotech.me>
2021-09-10 17:57:46 +0800

    feat 更新店家

--- src/pages/Menu/components/Store.js
+++ src/pages/Menu/components/Store.js
@@ -12,7 +12,6 @@ const Store = ({ storeDate }) => {
         <Image src="https://hsinchu.lakeshore.com.tw/wp-content/uploads/sites/12/2020/08/hs_mwr_gallery_3.jpg" fluid rounded />
       </Box>
       <h1>{storeDate.name}</h1>
-      {storeDate.rating}
       <p><StarRoundedIcon color="secondary"></StarRoundedIcon>{storeDate.rating}</p>
       <p>外送 {storeDate.deliveryTime} 分鐘</p>
     </div>
